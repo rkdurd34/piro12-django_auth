ca2dacf32d4b50588350e946bfcb66c93372fe30
rkdrud34 <rkdrud34@nate.com>
2020-01-18 00:16:39 +0900

    djangoask-applying_sql

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
