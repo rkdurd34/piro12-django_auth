f8f57ec3cd81ef72487008488fb353e25914bdf3
rkdrud34 <rkdrud34@nate.com>
2020-01-21 10:41:30 +0900

    djangoask-freshstatic

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'shop',
     'blog',
+    'common',
 ]
 
 MIDDLEWARE = [
--- askcompany/urls.py
+++ askcompany/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('shop/', include('shop.urls')),
+    path('',views.item_list)
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
