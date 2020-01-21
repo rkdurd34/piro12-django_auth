d331d36a5d61ebf75a7aaed2f12eacb52cc5cb07
rkdrud34 <rkdrud34@nate.com>
2020-01-21 12:04:10 +0900

    djangoask-edit_without_form

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404,redirect
 from io import BytesIO
@@ -53,3 +54,72 @@ def item_detail(request,pk):
     return render(request, 'shop/item_detail.html',{
         'item' : item
     })
+
+
+def item_new(request, item=None):
+    error_list = []
+    initial = {}
+
+    if request.method == 'POST':
+        data = request.POST
+        files = request.FILES
+
+        name = data.get('name')
+        desc = data.get('desc')
+        price = data.get('price')
+        photo = files.get('photo')
+        is_publish = data.get('is_publish') in (True, 't', 'True', '1')
+
+        # 유효성 검사
+        if len(name) < 2:
+            error_list.append('name을 2글자 이상 입력해주세요.')
+
+        if re.match(r'^[\da-zA-Z\s]+$', desc):
+            error_list.append('한글을 입력해주세요.')
+
+        if not error_list:
+            # 저장 시도
+            if item is None:
+                item = Item()
+
+            item.name = name
+            item.desc = desc
+            item.price = price
+            item.is_publish = is_publish
+
+            if photo:
+                item.photo.save(photo.name, photo, save=False)
+
+            try:
+                item.save()
+            except Exception as e:
+                error_list.append(e)
+            else:
+                return redirect(item)  #item.get_absolute_url이 호출됨
+
+        initial = {
+            'name': name,
+            'desc': desc,
+            'price': price,
+            'photo': photo,
+            'is_publish': is_publish,
+        }
+    else:
+        if item is not None:
+            initial = {
+                'name': item.name,
+                'desc': item.desc,
+                'price': item.price,
+                'photo': item.photo,
+                'is_publish': item.is_publish,
+            }
+
+    return render(request, 'shop/item_new.html', {
+        'error_list': error_list,
+        'initial': initial,
+    })
+
+
+def item_edit(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return item_new(request, item)
